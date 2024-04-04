feba133db81ae7c484b3d712628f15e689b54f82
israel <lisrael1989@gmail.com>
2024-04-04 17:30:29 +0300

    v1 of nav in images -need to finish

--- src/cmps/StayGallery.jsx
+++ src/cmps/StayGallery.jsx
@@ -1,33 +1,60 @@
 import { useState } from 'react'
-
-export function StayGallery({ imgUrls, isSaved, onSave }) {
+import { Link } from "react-router-dom"
+export function StayGallery({ stayId, imgUrls, isSaved, onSave }) {
   const [currentImgIndex, setCurrentImgIndex] = useState(0)
 
-  const nextImg = () => {
-    setCurrentImgIndex((prevIndex) => (prevIndex + 1) % imgUrls.length)
-  }
+  const nextImg = (ev) => {
+    ev.stopPropagation();
+    setCurrentImgIndex((prevIndex) => (prevIndex + 1) % imgUrls.length);
+  };
 
-  const prevImg = () => {
-    setCurrentImgIndex((prevIndex) => (prevIndex - 1 + imgUrls.length) % imgUrls.length)
-  }
+  const prevImg = (ev) => {
+    ev.stopPropagation();
+    setCurrentImgIndex((prevIndex) => (prevIndex - 1 + imgUrls.length) % imgUrls.length);
+  };
 
   const handleSaveClick = (ev) => {
     ev.stopPropagation();
     onSave();
   };
 
+  // return (
+  //   <div className="stay-gallery">
+  //     <img className='img-gallery ' src={imgUrls[currentImgIndex]} alt="Stay" />
+  //     <i
+  //       className={`fa-duotone fa-heart  ${isSaved ? 'fas' : 'far'}`}
+  //       onClick={handleSaveClick}
+
+  //     ></i>
+  //     <section className='btn-preview'>
+  //       <button onClick={prevImg}>Prev</button>
+  //     <button onClick={nextImg}>Next</button>
+  //     </section>
+  //   </div>
+  // )
+
   return (
     <div className="stay-gallery">
-      <img className='img-gallery ' src={imgUrls[currentImgIndex]} alt="Stay" />
+      <Link className="img-link" to={`/${stayId}`}>
+        <img className='img-gallery' src={imgUrls[currentImgIndex]} alt="Stay" /></Link>
       <i
         className={`fa-duotone fa-heart  ${isSaved ? 'fas' : 'far'}`}
         onClick={handleSaveClick}
 
       ></i>
       <section className='btn-preview'>
-        {/* <button onClick={prevImg}>Prev</button>
-      <button onClick={nextImg}>Next</button> */}
+        {imgUrls[currentImgIndex] !== imgUrls[0] ? <button className=" btn-nav-img prev" onClick={prevImg}></button> : <span></span>}
+        <button className="btn-nav-img  next" onClick={nextImg}></button>
       </section>
-    </div>
-  )
+      <div className="image-counter">
+        {imgUrls.map((url, index) => (
+          <div
+            key={index}
+            className={`dot ${index === currentImgIndex ? 'active' : ''}`}
+          />
+        ))}
+      </div>
+    </div >
+  );
+
 }
