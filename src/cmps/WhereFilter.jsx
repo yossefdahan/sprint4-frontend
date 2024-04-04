f1c5ce8404f2172f6b08fd55ebe55856175fea98
yossefdahan <yossefdahan1@gmail.com>
2024-04-04 17:37:17 +0300

    Add datePicker at the header

--- src/cmps/WhereFilter.jsx
+++ src/cmps/WhereFilter.jsx
@@ -2,6 +2,9 @@ import { useEffect, useRef, useState } from "react";
 import { utilService } from "../services/util.service";
 import { useEffectUpdate } from "../customHooks/useEffectUpdate";
 import { stayService } from "../services/stay.service.local";
+import DatePicker from 'react-datepicker'
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 
 
@@ -10,6 +13,13 @@ export function WhereFilter({ filterBy, onSetFilter }) {
     const [inputValue, setInputValue] = useState("")
     const [suggestions, setSuggestions] = useState([])
     const [allCountries, setAllCountries] = useState([])
+    const [countyModal, setCountryModal] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+    const [startDate, setStartDate] = useState(null)
+    const [endDate, setEndDate] = useState(null)
+    const datePickerRef = useRef(null)
+
+
     onSetFilter = useRef(utilService.debounce(onSetFilter, 300))
 
     useEffect(() => {
@@ -20,7 +30,11 @@ export function WhereFilter({ filterBy, onSetFilter }) {
         loadCountries()
     }, [])
 
-
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setIsOpen(true)
+        }
+    }
 
     useEffectUpdate(() => {
         onSetFilter.current({ ...filterByToEdit, loc: { country: inputValue } })
@@ -49,23 +63,96 @@ export function WhereFilter({ filterBy, onSetFilter }) {
         setSuggestions([])
 
         onSetFilter.current({ ...filterByToEdit, loc: { country: suggestion } })
+        setIsOpen(true)
     }
-    return (<section className="where-filter-container">
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        console.log('Submitted with startDate:', startDate, 'endDate:', endDate, 'location:', inputValue)
+
+        setIsOpen(false)
+        setCountryModal(false)
+        onSetFilter.current(filterByToEdit)
+    }
+
+    return (<section className="where-filter-container" >
         <h5>Where</h5>
-        <input
-            placeholder="Search destination"
-            type="text"
-            value={inputValue}
-            onChange={handleChange}
-        />
-        {inputValue && suggestions.length > 0 && (
-            <ul>
-                {suggestions.map(suggestion => (
-                    <li key={suggestion} onClick={() => handleSuggestionClick(suggestion)}>
-                        {suggestion}
-                    </li>
-                ))}
-            </ul>
-        )}
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="Search destination"
+                type="text"
+                value={inputValue}
+                onChange={handleChange}
+                onClick={() => setCountryModal(!countyModal)}
+            />
+            {countyModal && !inputValue &&
+                <div style={{ width: "100px", display: "flex" }} >
+                    <img onClick={() => {
+                        setInputValue("mexico")
+                        setIsOpen(true)
+                    }} src="src\assets\img\destination\asset 0.jpeg" alt="Im flexible" />
+                    <h4>Im flexible</h4>
+
+                    <img onClick={() => {
+                        setInputValue("portugal")
+                        setIsOpen(true)
+                    }} src="src\assets\img\destination\asset 2.webp" alt="Europe" />
+                    <h4>Europe</h4>
+
+                    <img onClick={() => {
+                        setInputValue("portugal")
+                        setIsOpen(true)
+                    }} src="src\assets\img\destination\asset 3.webp" alt="Italy" />
+                    <h4>Italy</h4>
+
+                    <img onClick={() => {
+                        setInputValue("canada")
+                        setIsOpen(true)
+                    }} src="src\assets\img\destination\asset 4.webp" alt="United States" />
+                    <h4>United States</h4>
+
+                    <img onClick={() => {
+                        setInputValue("usa")
+                        setIsOpen(true)
+                    }} src="src\assets\img\destination\asset 5.webp" alt="Greece" />
+                    <h4>Greece</h4>
+
+                    <img onClick={() => {
+                        setInputValue("portugal")
+                        setIsOpen(true)
+                    }} src="src\assets\img\destination\asset 1.webp" alt="South America" />
+                    <h4>South America</h4>
+
+                </div>
+            }
+
+            {inputValue && suggestions.length > 0 && (
+                <ul>
+                    {suggestions.map(suggestion => (
+                        <li key={suggestion} onClick={() => handleSuggestionClick(suggestion)}>
+                            {suggestion}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            {isOpen && (
+                <div tabIndex={0} onKeyDown={handleKeyDown}>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(dates) => {
+                            const [start, end] = dates
+                            setStartDate(start)
+                            setEndDate(end)
+                        }}
+                        startDate={startDate}
+                        endDate={endDate}
+                        selectsRange
+                        inline
+                        monthsShown={2}
+                    />
+                </div>
+            )}
+        </form>
     </section>)
 }
