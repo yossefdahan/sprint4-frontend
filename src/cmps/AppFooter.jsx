ee70d03f224835e98ed5af5d5fff9d7e74bc9d83
israel <lisrael1989@gmail.com>
2024-04-08 09:11:29 +0300

    remove some code from footer

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,25 +1,6 @@
-// export function AppFooter() {
-//     return (
-//         <div className="app-container">
-//             <footer className="app-footer">
-//                 <div className="second-section-footer">
-//                     <span> English (US) </span>
-//                     <span> $ USD </span>
-//                     <span> Support & resources </span>
-//                 </div>
-//                 <div className="footer-content">
-
-//                     © 2024 Airstay, Inc. · <span> Terms </span> · <span> Sitemap </span> · <span> Privacy </span>· <span> Your Privacy Choices </span> <img className="privacy-icon" src="src/assets/img/small-icons/asset 99.svg" alt="" />
-
-//                 </div>
-//             </footer>
-//         </div>
-//     );
-// }
-
 export function AppFooter() {
     return (
-        <footer className="app-footer full">
+        <footer className="app-footer">
             <div className="footer-content">
 
                 <div className="footer-main-content">
