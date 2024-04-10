99a3042818eb150b4b3ca4cd4544f055f6ceb573
israel <lisrael1989@gmail.com>
2024-04-10 13:25:18 +0300

    add payment modal

--- src/cmps/PaymentRequest.jsx
+++ src/cmps/PaymentRequest.jsx
@@ -20,8 +20,34 @@ export function PaymentRequest({ guests, order, stay, isOpen, setOpen }) {
         setButtonColor(`hsl(351, 83%, ${lightness}%)`);
     };
 
+    // async function dealMade(ev) {
+    //     ev.preventDefault();
 
+    //     try {
+    //         await addOrder(order);
+    //         setSend(true);
 
+    //         Swal.fire({
+    //             position: "center center",
+    //             icon: "success",
+    //             title: "Your work has been saved",
+    //             showConfirmButton: false,
+    //             timer: 1500
+    //         });
+    //         console.log('Order complete!');
+    //         setTimeout(() => {
+    //             toUserTrips();
+    //         }, 1500);
+    //     } catch (err) {
+    //         console.error(err);
+
+    //         Swal.fire({
+    //             title: 'Error!',
+    //             text: 'Something went wrong with your reservation.',
+    //             icon: 'error',
+    //         });
+    //     }
+    // }
 
     async function dealMade(ev) {
         ev.preventDefault();
@@ -33,7 +59,10 @@ export function PaymentRequest({ guests, order, stay, isOpen, setOpen }) {
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#ff385c',
-            confirmButtonText: 'Yes, confirm it!'
+            confirmButtonText: 'Yes, confirm it!',
+            didClose: () => {
+                document.body.style.overflow = '';
+            }
         });
 
         if (result.isConfirmed) {
@@ -41,12 +70,18 @@ export function PaymentRequest({ guests, order, stay, isOpen, setOpen }) {
                 await addOrder(order);
                 setSend(true);
                 Swal.fire({
+
                     position: 'center',
                     icon: 'success',
                     title: 'Reservation sent to confirmation',
                     text: 'Host will respond within 24 hours ',
                     showConfirmButton: false,
-                    timer: 3000
+                    backdrop: false,
+                    widthAuto: false,
+
+
+                    timer: 3000,
+
                 });
                 console.log('complete!');
                 setTimeout(() => {
@@ -57,11 +92,14 @@ export function PaymentRequest({ guests, order, stay, isOpen, setOpen }) {
                 Swal.fire({
                     title: 'Error!',
                     text: 'Something went wrong with your reservation.',
-                    icon: 'error'
+                    icon: 'error',
+
                 });
             }
         }
+
     }
+
     function toUserTrips() {
         window.location.href = '/user/trips'
     }
@@ -76,78 +114,82 @@ export function PaymentRequest({ guests, order, stay, isOpen, setOpen }) {
     }
 
     return (
-        <section className='background-opacity'>
-            <div open className='payment-confrim-modal'>
+        <section className='background-opacity flex'>
 
-                <h2>One last step</h2>
-                <section className='flex column'>
-                    <span>Dear Guest</span>
-                    <span>{'In order to complete your reservation , please confrim your trip details'}</span>
-                </section>
-
-                <h3> Your Trip</h3>
-                <section>
-
-                    <section className='flex space-between'>
-                        <span>Check-In</span>
-                        <span>{formatDateRange(order.startDate)}</span>
+            <div open className='payment-confrim-modal flex '>
+                <section className='left-payment-modal-section'>
+                    <h2>One last step</h2>
+                    <section className=' summery-payment-modal flex column'>
+                        <span>Dear Guest</span>
+                        <span className='p-summery-payment'>{'In order to complete your reservation , please confirm your trip details'}</span>
                     </section>
 
-                    <section className='flex space-between'>
-                        <span>Check-Out</span>
-                        <span>{formatDateRange(order.endDate)}</span>
+                    <h3 className='stay-details-title-payment-modal'>Stay details:</h3>
+                    <section className=' stay-details-section-payment-modal flex img-cont'>
+                        <img src={stay.imgUrls[0]} alt="" />
+                        <section className='title'>
+                            <h4>{stay.name}</h4>
+                            <span>Entire rental unit</span>
+                            <section className='rate-title'>
+                            </section>
+                            <span>★ {rate / stay.reviews.length} ({stay.reviews.length} reviews)</span>
+                        </section>
                     </section>
 
-                    <section className='flex space-between'>
-                        <span>Host name:</span>
-                        <span>{stay.host.fullname}</span>
+                    <section className="first-payment-modal-section column ">
+                        <h3> Your Trip</h3>
+                        <section className='flex space-between'>
+                            <span>Host name:</span>
+                            <span>{stay.host.fullname}</span>
+                        </section>
+
+                        <section className='flex space-between'>
+                            <span>Check-In</span>
+                            <span>{formatDateRange(order.startDate)}</span>
+                        </section>
+
+                        <section className='flex space-between'>
+                            <span>Check-Out</span>
+                            <span>{formatDateRange(order.endDate)}</span>
+                        </section>
                     </section>
+
                 </section>
 
                 <section className='title-container'>
-                    <hr />
-                    <div className='flex column'>
-                        <h3>Stay details:</h3>
-                        <section className='flex img-cont'>
-                            <img src={stay.imgUrls[0]} alt="" />
-                            <section className='title'>
-                                <h4>{stay.name}</h4>
-                                <span>Entire rental unit</span>
-                                <section className='rate-title'>
-                                </section>
-                                <span>★{rate / stay.reviews.length} ({stay.reviews.length} reviews)</span>
-                            </section>
+                    <div className="second-section-payment-modal">
+                        <h3>Price details</h3>
+                        <section className='flex space-between'>
+                            <span>$ {order.stay.price} X {Math.floor(order.totalPrice / order.stay.price)} nights </span>
+                            <span>$ {order.stay.price * Math.floor(order.totalPrice / order.stay.price)}</span>
+                        </section >
+                        <section className='flex space-between'>
+                            <span>Guests</span>
+                            <span> {adults + children + infants + pets} </span>
+                        </section>
+                        <section className='flex space-between'>
+                            <span>Cleaning fee </span>
+                            <span>$ {(order.stay.price * Math.floor(order.totalPrice / order.stay.price)) / 10}</span>
+                        </section>
+                        <section>
+
+                        </section>
+                        <hr />
+                        <section className='flex space-between'>
+                            <h3>Total (USD) </h3>
+                            <span>$ {order.totalPrice}</span>
+                        </section>
+                        <section className='flex space-between close-confirm-btn'>
+                            <button className='close-btn-payment' onMouseMove={handleMouseMove} onMouseOut={() => setButtonColor('#ff385c')} onClick={() => setOpen(!isOpen)}>Close</button>
+                            <button className="confirm-btn-payment" onMouseMove={handleMouseMove} onMouseOut={() => setButtonColor('#ff385c')} onClick={dealMade}>Confirm</button>
                         </section>
 
-                    </div>
-                    <hr />
-                    <h3>Price details</h3>
-                    <section className='flex space-between'>
-                        <span>$ {order.stay.price} X {Math.floor(order.totalPrice / order.stay.price)} nights </span>
-                        <span>$ {order.stay.price * Math.floor(order.totalPrice / order.stay.price)}</span>
-                    </section >
-                    <section className='flex space-between'>
-                        <span>Guests</span>
-                        <span> {adults + children + infants + pets} </span>
-                    </section>
-                    <section className='flex space-between'>
-                        <span>Cleaning fee </span>
-                        <span>$ {(order.stay.price * Math.floor(order.totalPrice / order.stay.price)) / 10}</span>
-                    </section>
-                    <section>
 
-                    </section>
-                    <hr />
-                    <section className='flex space-between'>
-                        <h3>Total (USD) </h3>
-                        <span>$ {order.totalPrice}</span>
-                    </section>
-                    <section className='flex space-between close-confirm-btn'>
-                        <button onMouseMove={handleMouseMove} onMouseOut={() => setButtonColor('#ff385c')} onClick={() => setOpen(!isOpen)}>Close</button>
-                        <button onMouseMove={handleMouseMove} onMouseOut={() => setButtonColor('#ff385c')} onClick={dealMade}>Confirm</button>
-                    </section>
+                    </div>
+
                 </section>
-            </div>
-        </section>
+            </div >
+
+        </section >
     )
 }
