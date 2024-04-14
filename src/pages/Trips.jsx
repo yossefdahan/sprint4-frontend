1526f91ab886746e638bfcf3edb47fead2a783c2
israel <lisrael1989@gmail.com>
2024-04-14 20:11:57 +0300

    adding sort by new trips to trip page

--- src/pages/Trips.jsx
+++ src/pages/Trips.jsx
@@ -1,127 +1,4 @@
 
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { loadOrders } from '../store/order.actions';
-// import { NavLink } from 'react-router-dom';
-// import { utilService } from '../services/util.service';
-// import { SocialIconsTrips } from '../cmps/SocialIconsTrips'
-// // import israelImg from '../assets/Img/israel.jpg'
-// import { loadUsers } from '../store/user.actions';
-// import { userService } from '../services/user.service';
-// import { loadStays } from '../store/stay.actions';
-
-
-// export function Trips() {
-
-//     const orders = useSelector((storeState) => storeState.orderModule.orders)
-//     const users = useSelector(storeState => storeState.userModule.users)
-//     const stays = useSelector(storeState => storeState.stayModule.stays)
-//     const loggedinUser = userService.getLoggedinUser()
-//     useEffect(() => {
-//         loadOrders()
-//         loadUsers()
-//         // loadStays()
-//     }, [])
-
-//     if (!orders || !users || !loggedinUser || !stays) return <div>No orders founds..</div>
-
-//     const filteredOrders = orders.filter(order => {
-//         const stay = stays.find(stay => stay._id === order.stay._id)
-//         return stay && loggedinUser._id === order.buyer._id
-//     })
-
-//     function handleAddToCalendar(order, stay) {
-
-//         const startDate = utilService.formatIsoDateToYMD(order.startDate).replace(/-/g, '');
-//         const endDate = utilService.formatIsoDateToYMD(order.endDate).replace(/-/g, '');
-//         const startTime = 'T000000';
-//         const endTime = 'T235959';
-//         const details = encodeURIComponent(`Stay at ${order.stay.name} with ${order.guests.adults} adults, ${order.guests.kids || 0} kids.`);
-//         const location = encodeURIComponent(`${stay.loc.city}, ${stay.loc.country}`);
-//         const googleCalendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=Trip to ${order.stay.name}&dates=${startDate}${startTime}/${endDate}${endTime}&details=${details}&location=${location}&sf=true&output=xml`;
-
-//         window.open(googleCalendarUrl, '_blank');
-//     }
-
-
-//     return (
-//         <div className="trips-page">
-//             <div className="navigation-links">
-//                 <NavLink to="/user/trips" activeClassName="active">Trips</NavLink>
-//                 {/* {  <NavLink to="/user/dashboard" activeClassName="active">Dashboard</NavLink>} */}
-//             </div>
-
-//             <div className="social-icons-container">
-//                 <SocialIconsTrips />
-//             </div>
-//             <section>
-//                 <section>
-//                     <h1></h1>
-//                 </section>
-//             </section>
-//             <div className='dashboard-container'>
-
-//                 <div className="trips-container ">
-
-//                     {filteredOrders.map((order) => {
-//                         const stay = stays.find(stay => stay._id === order.stay._id)
-//                         // if (!stay) return null
-
-//                         return (
-//                             <div key={order._id} className="trip-card">
-//                                 <div>
-//                                     <button className="add-to-calender" onClick={() => handleAddToCalendar(order)}>
-//                                         <svg xmlns="http://www.w3.org/2000/svg"
-//                                             width="24" viewBox="0 0 24 24"
-//                                             height="24"
-//                                             fill="none"
-//                                             className="svg-icon">
-//                                             <g stroke-width="2" stroke-linecap="round" stroke="#fff">
-//                                                 <rect y="5" x="4" width="16" rx="2" height="16"></rect>
-//                                                 <path d="m8 3v4"></path><path d="m16 3v4"></path>
-//                                                 <path d="m4 11h16"></path></g></svg>
-//                                         <span className="lable">Add to Calendar</span></button>
-//                                 </div>
-//                                 <div className='trip-card-header flex space-between'>
-//                                     <div>
-//                                         <h2 className='stay-name-trips-card'>{stay.name}</h2>
-//                                         <p><strong className='location-trips-card'> {stay.loc.city}, {stay.loc.country}</strong> </p>
-//                                     </div>
-//                                     <div className='right-section-header-trips flex column align-center'>
-//                                         <img className="israel-img" src={stay.host.imgUrl} alt="Host" />
-//                                         <p className='host-name-trips'>{stay.host.fullname}</p>
-//                                     </div>
-//                                 </div>
-//                                 <div className='details-section-trips flex space-between align-center'>
-//                                     <div>
-//                                         <p><strong className='room-type-trips-card'> Stay Type:</strong> {stay.roomType}</p>
-//                                         <p><strong className='start-date-trips-card'>Start Date:</strong> {utilService.formatIsoDateToYMD(order.startDate)}</p>
-//                                         <p><strong className='end-dates-trips-card'>End Date:</strong> {utilService.formatIsoDateToYMD(order.endDate)}</p>
-//                                         <p>
-//                                             <strong className='guests-trips-card'>Guests:</strong>
-//                                             {order.guests.adults ? ` Adults: ${order.guests.adults}` : ''}
-//                                             {order.guests.kids ? ` | Kids: ${order.guests.kids}` : ''}
-//                                             {order.guests.pets ? ` | Pets: ${order.guests.pets}` : ''}
-//                                             {order.guests.infants ? ` | Infants: ${order.guests.infants}` : ''}
-//                                         </p>
-//                                     </div>
-//                                     <div className='img-trips'>
-
-//                                         <img className='trips-stay-img' src={stay.imgUrls[0]} alt="Stay" />
-//                                     </div>
-//                                 </div>
-//                                 <div className='card-trips-footer flex space-between'>
-//                                     <p><strong className='price-trips-card'>Total Price:</strong>  <span>${order.totalPrice.toFixed(2)}</span></p>
-//                                     <p className={`status ${order.status}`}><strong>Status:</strong> {order.status}</p>
-//                                 </div>
-//                             </div>
-//                         )
-//                     })}
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadOrders } from '../store/order.actions';
@@ -151,6 +28,9 @@ export function Trips() {
         return stay && loggedinUser._id === order.buyer._id;
     });
 
+    const sortedOrders = filteredOrders.sort((a, b) => b.totalPrice - a.totalPrice);
+
+
     function handleAddToCalendar(order, stay) {
         if (!stay) {
             console.error('Stay details are missing');
@@ -173,7 +53,7 @@ export function Trips() {
 
     if (filteredOrders.length === 0) {
         return <div className='loading-bar flex align-center'>
-            <div className='loading-trips-title'>No orders founds..</div>
+            <div className='loading-trips-title'>Loading..</div>
             <section class="loader-trips">
                 <div>
                     <div>
