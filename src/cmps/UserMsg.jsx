923105257cb991e6596c17954ab8a760e759e664
bengold1999 <bengold1999@gmail.com>
2024-04-15 18:03:42 +0300

    Add socket

--- src/cmps/UserMsg.jsx
+++ src/cmps/UserMsg.jsx
@@ -1,6 +1,6 @@
 import { eventBus, showSuccessMsg } from "../services/event-bus.service.js"
 import { useState, useEffect, useRef } from 'react'
-import { socketService, SOCKET_EVENT_REVIEW_ABOUT_YOU } from "../services/socket.service.js"
+import { socketService, SOCKET_EVENT_ORDER_STATUS } from "../services/socket.service.js"
 
 export function UserMsg() {
 
@@ -18,13 +18,13 @@ export function UserMsg() {
       timeoutIdRef.current = setTimeout(closeMsg, 3000)
     })
 
-    socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, (review) => {
+    socketService.on(SOCKET_EVENT_ORDER_STATUS, (review) => {
       showSuccessMsg(`New review about me ${review.txt}`)
     })
 
     return () => {
       unsubscribe()
-      socketService.off(SOCKET_EVENT_REVIEW_ABOUT_YOU)
+      socketService.off(SOCKET_EVENT_ORDER_STATUS)
     }
   }, [])
 
--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -7,7 +7,7 @@ export const SOCKET_EMIT_SET_TOPIC = 'chat-set-topic'
 export const SOCKET_EMIT_USER_WATCH = 'user-watch'
 export const SOCKET_EVENT_USER_UPDATED = 'user-updated'
 export const SOCKET_EVENT_REVIEW_ADDED = 'review-added'
-export const SOCKET_EVENT_REVIEW_ABOUT_YOU = 'review-about-you'
+export const SOCKET_EVENT_ORDER_STATUS = 'order-status'
 
 const SOCKET_EMIT_LOGIN = 'set-user-socket'
 const SOCKET_EMIT_LOGOUT = 'unset-user-socket'
@@ -15,7 +15,7 @@ const SOCKET_EMIT_LOGOUT = 'unset-user-socket'
 
 const baseUrl = (process.env.NODE_ENV === 'production') ? '' : '//localhost:3030'
 // export const socketService = createSocketService()
-export const socketService = createDummySocketService()
+export const socketService = createSocketService()
 
 // for debugging from console
 window.socketService = socketService
