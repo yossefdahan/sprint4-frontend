05314d5bc452c66f7132a534ac8f082aa2f4ad23
bengold1999 <bengold1999@gmail.com>
2024-05-30 13:21:52 +0300

    Add Dashboard to footer

--- src/cmps/FooterPhone.jsx
+++ src/cmps/FooterPhone.jsx
@@ -21,6 +21,8 @@ export function FooterPhone() {
         navigate('/')
     }
 
+
+
     return (<>
         {isModalOpen && <LoginSignup onClose={toggleModal} />}
 
@@ -30,7 +32,7 @@ export function FooterPhone() {
                 {user ? (<Link to="/user/trips" className="footer-nav-link"> <i className="fa-brands fa-airbnb"></i>Trips</Link>) : ''}
                 {user ? <Link to="/user/addstay" className="footer-nav-link"><i className="fa-solid fa-house-flag"></i>Add Stay </Link> : ''}
                 {user ? <div onClick={() => onLogout()} className='footer-nav-link'><i className="fa-solid fa-user"></i>Logout</div> : <div onClick={toggleModal} className='footer-nav-link'><i className="fa-solid fa-user"></i>Login</div>}
-                {/* <Link to="/user/dashboard" className="  footer-nav-link"> <i className="fa-solid fa-chart-line"></i>Dashboard</Link> */}
+                {user?<Link to="/user/dashboard" className="  footer-nav-link"> <i className="fa-solid fa-chart-line"></i>Dashboard</Link>:''}
             </nav>
 
         </footer>
